682ce63e5a1f581f73b4eedda09828686b55699e
Vipin Chauhan <110002254+vipin787@users.noreply.github.com>
2023-04-13 11:56:49 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const fileContent = "Newton School";
 const writeFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
+  await fs.writeFile(fileName, fileContent)
 };
 
 module.exports = { writeFile };
